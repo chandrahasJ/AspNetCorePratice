16bb27189685bc3be9b95742b070c125c9eae6ea
chandrahasJ <chandrahasJ@users.noreply.github.com>
2018-07-24 07:01:43 +0530

    ViewBag & Perviously OCP AutoMapper
    1) ViewBag
    2) Perviously Created OCP AutoMapper

---
+++ Project2_ViewBagPassingCToV/Project2_ViewBagPassingCToV/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project2_ViewBagPassingCToV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Chandrahas Poojari";
+            ViewBag.Birthday = DateTime.Parse("7/7/1988");
+            ViewBag.IsMarried = true;
+            ViewBag.Salary = 5000000000000000000;
+
+            return View();
+        }
+    }
+}
