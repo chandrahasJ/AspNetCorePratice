f078b6c971981f7370414359e0a120ba39fea6ec
chandrahasJ <chandrahasJ@users.noreply.github.com>
2018-07-26 22:41:34 +0530

    Read from AppSettings.json
    Read from AppSettings.json

---
+++ Project3_ReadAppConfig/Project3_ReadAppConfig/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Project3_ReadAppConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json");
+            var configurationData = builder.Build();
+
+            ViewBag.results1 = configurationData["Message"];
+            ViewBag.ConfigProperties1 = configurationData["MyConfigData:ConfigData1"];
+            ViewBag.ConfigProperties2 = configurationData["MyConfigData:ConfigData2"];
+            ViewBag.ConfigProperties3 = configurationData["MyConfigData:ConfigData3"];
+            return View();
+        }
+    }
+}
