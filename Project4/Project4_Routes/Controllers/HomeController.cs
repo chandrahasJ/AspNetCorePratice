d1d3cea305af4a38ca8f7f2905e4b4a8780d24e3
chandrahasJ <chandrahasJ@users.noreply.github.com>
2018-07-26 23:19:46 +0530

    Route Part 1
    Route Part 1

---
+++ Project4/Project4_Routes/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project4_Routes.Controllers
+{
+    [Route("demo")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("demo2")]
+        public IActionResult Demo2()
+        {
+            return View();
+        }
+    }
+}
