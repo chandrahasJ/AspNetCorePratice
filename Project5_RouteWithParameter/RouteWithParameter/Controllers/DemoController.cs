b3a028030c3b7e6b52e811cc75ee1d84a5416bca
chandrahasJ <38713303+chandrahasJ@users.noreply.github.com>
2018-08-18 08:33:13 +0530

    Project4 - Project 5
    Routes
    Routes with parameter

---
+++ Project5_RouteWithParameter/RouteWithParameter/Controllers/DemoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouteWithParameter.Controllers
+{
+    [Route("demo")]
+    public class DemoController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("MyDemoPage")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("demo2/{id}")]
+        [Route("cj/{id}")]
+        public IActionResult Demo2(int id)
+        {
+            ViewBag.id = id;
+            return View("Demo2");
+        }
+
+        [Route("demo3/{id1}/{id2}")]
+        [Route("cc/{id1}/{id2}")]
+        public IActionResult Demo3(int id1,string id2)
+        {
+            ViewBag.id = id1;
+            ViewBag.id2 = id2;
+            return View("Demo3");
+        }
+    }
+}
